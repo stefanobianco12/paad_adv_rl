eb390e5a6a7ed60cceb12cb6cb5475e882db7a11
stefanobianco12 <s.bianchettin@gmail.com>
2024-10-09 18:37:30 +0200

    fix cuda

--- code_atari/paad_rl/scripts/train/dqn_train_attacker.sh
+++ code_atari/paad_rl/scripts/train/dqn_train_attacker.sh
@@ -8,6 +8,7 @@ CUDA=$2
 WEIGHT_1=$3
 
 WEIGHT_2=$4
+CUDA_VISIBLE_DEVICES=${CUDA}
 
 # path to the trained victim model. 
 VICTIM_PATH="./trained_models/dqn_victim/" ## use this line if attack our pre-trained victim 
@@ -114,8 +115,8 @@ fi
 ########################################
 
 ### train and test pa-ad attack ########
-CUDA_VISIBLE_DEVICES=${CUDA} python trainer_adv/dqn_pa_attacker.py --env-name ${ENV} --algo ppo --epsilon ${EPS} --cuda-id ${CUDA} --num-env-steps ${STEPS} --num-steps ${HORIZON} --num-processes ${NPROC} --use-linear-lr-decay --fgsm --res-dir ${DIR} --log-dir ${LOG} --victim-dir ${VICTIM_PATH} --weight_1 ${WEIGHT_1} --weight_2 ${WEIGHT_2}
-CUDA_VISIBLE_DEVICES=${CUDA} python evaluator/dqn_test.py --env-name ${ENV} --algo ppo --cuda-id ${CUDA} --attacker paad --epsilon ${EPS} --fgsm --res-dir ${DIR} --log-dir ${LOG} --det --victim-dir ${VICTIM_PATH} --test-episodes ${TEST_NUM}
+python trainer_adv/dqn_pa_attacker.py --env-name ${ENV} --algo ppo --epsilon ${EPS} --cuda-id ${CUDA} --num-env-steps ${STEPS} --num-steps ${HORIZON} --num-processes ${NPROC} --use-linear-lr-decay --fgsm --res-dir ${DIR} --log-dir ${LOG} --victim-dir ${VICTIM_PATH} --weight_1 ${WEIGHT_1} --weight_2 ${WEIGHT_2}
+python evaluator/dqn_test.py --env-name ${ENV} --algo ppo --cuda-id ${CUDA} --attacker paad --epsilon ${EPS} --fgsm --res-dir ${DIR} --log-dir ${LOG} --det --victim-dir ${VICTIM_PATH} --test-episodes ${TEST_NUM}
 ########################################
 
 ### train and test sa-rl attack ########
